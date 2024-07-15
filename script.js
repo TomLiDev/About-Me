7696532a2058f091222b33403990479a7ded7030
TomLiDev <t.litchfield00@gmail.com>
2024-07-15 17:42:23 +0100

    Refactor javascript functions

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Inside 1st")
     if (document.getElementsByTagName("body")[0].id === "home") {
         let sections = document.getElementsByClassName("timeline-button")
-        console.log(sections)
-        console.log("Only if home")
+        let details = document.getElementsByClassName("job-detail")
         sessionStorage.setItem("sectionVisible", "no")
         for (i = 0; i < sections.length; i++) {
             sections[i].addEventListener("click", checkVisible)
+            details[i].addEventListener("click", checkVisible)
         }
+
     }
 	}
 );
@@ -24,42 +25,31 @@ already displayed. If there is, then clicking the section will hide the currentl
 function checkVisible() {
     console.log("Inside check")
     job = this.id+"-detail"
+    let details = document.getElementsByClassName("job-detail")
+
     if (sessionStorage.getItem("sectionVisible") == "no") {
-        if ($(window).width()>=400) {
             showDetail(job)
         }
-        else {
-            showDetailMobile(job)
-        }
-    } else {
-        for (i = 0; i < sections.length; i++) {
-            $(sections[i]).fadeOut("slow");
+    else {
+        console.log("Inside fade out part")
+        for (i = 0; i < details.length; i++) {
+            $(details[i]).fadeOut("slow");
         }
         sessionStorage.setItem("sectionVisible", "no")
+        console.log(sessionStorage.getItem("sectionVisible"))
     }
 }
 
 
 function showDetail(job) {
-    sections = document.getElementsByClassName("job-detail");
     console.log(job)
-    for (i = 0; i < sections.length; i++) {
-        if (sections[i].id === job) {
+    let details = document.getElementsByClassName("job-detail")
+    for (i = 0; i < details.length; i++) {
+        if (details[i].id === job) {
             console.log("yes")
-            $(sections[i]).fadeIn("slow")
-            sections[i].style.display = "flex";
+            $(details[i]).fadeIn("slow")
             sessionStorage.setItem("sectionVisible", "Yes")
+            console.log(sessionStorage.getItem("sectionVisible"))
         }
     }
 }
-
-function showDetailMobile(job) {
-    sections = document.getElementsByClassName("job-detail");
-    for (i = 0; i < sections.length; i++) {
-        if (sections[i].id === job) {
-            $(sections[i]).fadeIn("slow")
-            sessionStorage.setItem("sectionVisible", "Yes")
-        }
-    }
-}
-
